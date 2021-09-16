353915534e11fbf745510f8d4b65aacb2047642f
KLIM_nk01 <KLIM_NIKITA_2001@mail.ru>
2021-09-16 23:33:23 +0300

    fix AppStyle

--- src/components/MedCentersPage/MedCentersMap/MedCentersMapStyle.tsx
+++ src/components/MedCentersPage/MedCentersMap/MedCentersMapStyle.tsx
@@ -5,6 +5,7 @@ export const MedCentersMapStyle = styled.div`
   width: 60%;
   height: 100%;
   color: ${STYLE_CONSTANTS.COLORS.black};
+ 
 `;
 export const MarkerStyle = styled.img`
   width: 30px;
